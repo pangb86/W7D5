c4251e8e6b66d9091da51df35b0c5b04fb80ecda
Bo Pang <pangb86@gmail.com>
2017-11-17 16:55:52 -0800

    Finished auth backend

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    password_hash = BCrypt::Password.new(self.password_digest)
+    password_hash.is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    @user = User.find_by(username: username)
+
+    return nil if @user.nil?
+
+    @user.valid_password?(password) ? @user : nil
+  end
+end
